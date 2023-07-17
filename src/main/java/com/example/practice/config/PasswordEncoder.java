ef05923a2a14e5bf077a2fef129319ede2c0cb6f
Kamal Shrestha <kamal@lancemeup.com>
2023-07-17 19:01:58 +0545

    Update PasswordEncoder.java

--- src/main/java/com/example/practice/config/PasswordEncoder.java
+++ src/main/java/com/example/practice/config/PasswordEncoder.java
@@ -9,15 +9,15 @@ public class PasswordEncoder {
     @Value("${password.salt}")
     private String salt;
 
-    public String encodePassword(String password){
-        BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
-        String saltedPassword=password+salt;
+    public String encodePassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String saltedPassword = password + salt;
         return passwordEncoder.encode(saltedPassword);
     }
 
-    public boolean matches(String enteredPassword,String storedHashedPassword){
-        BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
-        String saltedEnteredPassword=enteredPassword+salt;
-        return passwordEncoder.matches(saltedEnteredPassword,storedHashedPassword);
+    public boolean matches(String enteredPassword, String storedHashedPassword) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String saltedEnteredPassword = enteredPassword + salt;
+        return passwordEncoder.matches(saltedEnteredPassword, storedHashedPassword);
     }
 }
