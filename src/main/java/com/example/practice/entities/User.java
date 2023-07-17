7e27988ef4f2a2b02624891e1ff64f99913051bf
Kamal Shrestha <kamal@lancemeup.com>
2023-07-17 19:04:11 +0545

    user entity created

--- src/main/java/com/example/practice/entities/User.java
+++ src/main/java/com/example/practice/entities/User.java
@@ -1,11 +1,12 @@
 package com.example.practice.entities;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
