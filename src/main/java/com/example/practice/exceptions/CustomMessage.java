9d3180d9b55d97a38a699392f70db28f6d396326
Kamal Shrestha <kamal@lancemeup.com>
2023-07-19 01:43:24 +0545

    Create CustomMessage.java

---
+++ src/main/java/com/example/practice/exceptions/CustomMessage.java
@@ -0,0 +1,10 @@
+package com.example.practice.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CustomMessage {
+   public String message;
+}
