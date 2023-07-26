b4a47521a322465f4122d35cc309cc3daa331899
Kamal Shrestha <kamal@lancemeup.com>
2023-07-26 16:44:48 +0545

    Update SecurityConfiguration.java

--- src/main/java/com/example/practice/config/SecurityConfiguration.java
+++ src/main/java/com/example/practice/config/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .antMatchers("/api","/api/**").permitAll()
