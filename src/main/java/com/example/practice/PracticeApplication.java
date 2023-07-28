bfa37c16a34cbdb5af20197f3c5307f72d940eaa
Kamal Shrestha <kamal@lancemeup.com>
2023-07-28 23:57:13 +0545

    swagger setup

--- src/main/java/com/example/practice/PracticeApplication.java
+++ src/main/java/com/example/practice/PracticeApplication.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,11 +20,29 @@ public class PracticeApplication {
     public OpenAPI customopenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Rest API")
-                        .description("Your API Description")
-                        .version("1.0")
-                        .termsOfService("")
-                        .contact(new Contact().name("rest api").url("rest api"))
+                        .title(title)
+                        .description(description)
+                        .version(version)
+                        .termsOfService(termsOfServiceUrl)
+                        .contact(new Contact().name(contactName).url(contactUrl))
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
+
+    @Value("${swagger.api.title}")
+    private String title;
+
+    @Value("${swagger.api.description}")
+    private String description;
+
+    @Value("${swagger.api.version}")
+    private String version;
+
+    @Value("${swagger.api.termsOfServiceUrl}")
+    private String termsOfServiceUrl;
+
+    @Value("${swagger.api.contact.name}")
+    private String contactName;
+
+    @Value("${swagger.api.contact.url}")
+    private String contactUrl;
 }
